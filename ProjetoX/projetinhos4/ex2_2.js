fcffe51bd97add72553f35eb5bba0decc64b017b
Lucio Costa <luciosdc@outlook.com>
2023-07-22 21:23:48 -0300

    alteração

--- ProjetoX/projetinhos4/ex2_2.js
+++ ProjetoX/projetinhos4/ex2_2.js
@@ -1,21 +1,11 @@
-// criar referência ao form e aoS elemento h3 e h4 (resposta)
+// cria referência ao form e ao elemento h3 (onde será exibida resposta)
 const frm = document.querySelector("form")
-const resp1 = document.querySelector("h3")
-const resp2 = document.querySelector("h4")
+const resp = document.querySelector("h3")
 
-
-//Cria um "Ouvinte" de evento, acionado quando o botão submit for clicado
+// cria um "ouvinte" de evento, acionado quando o botão submit for clicado
 frm.addEventListener("submit", (e) => {
-    const titulo = frm.inTitulo.value // obtém o conteúdos dos campos
-    const duracao = Number(frm.inDuracao.value) // obtém conteúdos dos campos
+    const valor = Number(frm.inValor.value)
+    const tempo = Number(frm.inTempo.value)
 
-    const horas = Math.floor(duracao / 60) //arredonda para baixo resultado
-    const minutos = duracao % 60 // obtém o resto da divisão
-
-    resp1.innerText = titulo // exibe as respostas
-    resp2.innerText = `${horas} hora(s) e ${minutos} minutos(s)` //exibe a resposta do programa
-    
-    e.preventDefault() //evita envio do form
+    const valor
 })
-
-/*  */
