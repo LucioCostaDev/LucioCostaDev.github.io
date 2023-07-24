c6d37cd540956d059d1d5413b9dd1eff5e3ddf4a
Lucio Costa <luciosdc@outlook.com>
2023-07-24 00:48:55 -0300

    altualização

--- ProjetoX/projetinhos5/supermercado.js
+++ ProjetoX/projetinhos5/supermercado.js
@@ -1,4 +1,21 @@
 // cria uma referência ao form e os elementos de reposta (pelo seu id)
 const frm = document.querySelector("form")
-const resp1 = document.querySelector("resp1")
-const resp2 = document.querySelector("resp2")
+const resp1 = document.querySelector("#outPromocao")
+const resp2 = document.querySelector("#outPrecoTerceiro")
+
+// cria um "ouvinte" de evento, acionando quando o botão submit for clicado
+frm.addEventListener("submit", (e) => {
+    const desconto = Number(frm.inPreco.value) // obtém conteúdos dos campos de entrada
+    const produto = frm.improduto.value;
+    const preco = Number(frm.inPreco.value);
+
+    //calcula valores
+    const terceiro = peso / 2;
+    const total = (preco * 2) + tereceiro;
+
+    //exibe resposta
+    resp1.innerText = `${produto} - Promoção: Leve 3 por r$:${total.toFixed(2)}`;
+
+    e.preventDefault() // evita envio do form
+    
+})
