a5fc07903255bd091f986108abf27d1afe9370c9
Lucio Costa <luciosdc@outlook.com>
2023-07-24 00:54:18 -0300

    atualização

--- ProjetoX/projetinhos5/supermercado.js
+++ ProjetoX/projetinhos5/supermercado.js
@@ -5,16 +5,17 @@ const resp2 = document.querySelector("#outPrecoTerceiro")
 
 // cria um "ouvinte" de evento, acionando quando o botão submit for clicado
 frm.addEventListener("submit", (e) => {
-    const desconto = Number(frm.inPreco.value) // obtém conteúdos dos campos de entrada
-    const produto = frm.improduto.value;
+     // obtém conteúdos dos campos de entrada
+    const produto = frm.inProduto.value;
     const preco = Number(frm.inPreco.value);
 
     //calcula valores
-    const terceiro = peso / 2;
-    const total = (preco * 2) + tereceiro;
+    const terceiro = preco / 2;
+    const total = (preco * 2) + terceiro;
 
     //exibe resposta
-    resp1.innerText = `${produto} - Promoção: Leve 3 por r$:${total.toFixed(2)}`;
+    resp1.innerText = `${produto} - Promoção: Leve 3 por R$: ${total.toFixed(2)}`;
+    resp2.innerText = `O 3º produto custa apenas R$: ${terceiro.toFixed(2)}`;
 
     e.preventDefault() // evita envio do form
     
