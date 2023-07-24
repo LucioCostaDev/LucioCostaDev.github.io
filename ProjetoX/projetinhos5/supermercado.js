786dd1d3916514d7e3335e5e9e593c39fd1658e6
Lucio Costa <luciosdc@outlook.com>
2023-07-23 21:57:05 -0300

    atualização

---
+++ ProjetoX/projetinhos5/supermercado.js
@@ -0,0 +1,4 @@
+// cria uma referência ao form e os elementos de reposta (pelo seu id)
+const frm = document.querySelector("form")
+const resp1 = document.querySelector("resp1")
+const resp2 = document.querySelector("resp2")
