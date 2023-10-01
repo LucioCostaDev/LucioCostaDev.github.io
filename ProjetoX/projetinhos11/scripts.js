45b3b5cba62a7f31224f742b6be0ca76f7bd33fe
Lucio Costa <luciosdc@outlook.com>
2023-10-01 00:14:26 -0300

    atualizado projeto
    projeto finalizado

--- ProjetoX/projetinhos11/scripts.js
+++ ProjetoX/projetinhos11/scripts.js
@@ -3,10 +3,14 @@ const lightbox = document.querySelector(".lightbox");
 const lightboxImage = document.querySelector(".lightbox-image");
 const lightboxClose = document.querySelector(".lightbox-close");
 
+// vai ser utilizado o for each para percorrer cada item utilizando arrow 
 galleryItems.forEach((item) => {
+  // criado evento de clique e cada um com seu evento pessoal 
   item.addEventListener("click", () => {
+    // aqui e uma forma de pegar pelo date-src 
     const imageUrl = item
       .querySelector(".gallery-image")
+      // get pegar 
       .getAttribute("data-src");
     lightboxImage.setAttribute("src", imageUrl);
     lightbox.style.display = "flex";
