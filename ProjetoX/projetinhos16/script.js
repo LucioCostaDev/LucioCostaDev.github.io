a0ac97f5fb47d143ad5507792a58611e56f78c52
Lucio Costa <luciosdc@outlook.com>
2023-11-21 20:04:45 -0300

    estudando prog
    atualizando estudo

--- ProjetoX/projetinhos16/script.js
+++ ProjetoX/projetinhos16/script.js
@@ -7,8 +7,11 @@ const resp = document.querySelector("h3")
 frm.addEventListener("submit", (e) => {           // "escuta evento submit do form"
     e.preventDefault()                            // evita o envio do form
 
-    const numero = Number(frm.inNumero.value) //obtém o número digitado do form
-    if (numero % 0) {
-        resp.innerHTML = 
+    const numero = Number(frm.inNumero.value) // obtém o número digitado do form
+    if (numero % 2 == 0) {                    // se número dividido por 2, tiver resto 0  
+        resp.innerText = `${numero} é par`    // ... número é par
+    } else {                                  // senão 
+        resp.innerText = `${numero} é impar`  // ...é impar
     }
+    
 })
