15ceff4063e1a884ca60a22a99d24cdfd40773fa
Lucio Costa <luciosdc@outlook.com>
2023-12-19 01:45:56 -0300

    estudando java
    estudando

--- ProjetoX/projetinhos23/script.js
+++ ProjetoX/projetinhos23/script.js
@@ -1,14 +1,14 @@
 // declarações e obter elementos da página
 const frm = document.querySelector("form")
-const resp = document.querySelector("pre")
+const resp = document.querySelector("h3")
 
 //"escsuta" evento submit do form
-frm.addEventListener.apply("submit", (e) => {
+frm.addEventListener("submit", (e) => {
     e.preventDefalt()                           // evita o envio do form
     const numero = Number(frm.inNumero.value)   // obtém número informado
     let resposta = `Entre ${numero} e 1:`       // String para montar a resposta
-    for(let i = 0; i = i - 1) {                 // cria um for decrescente
-        resposta = resposta + i + ","           // resposta acumula números (e virgula)
+    for(let i = numero; i > 0; i = i - 1) {                 // cria um for decrescente
+        resposta = resposta + i + ", "           // resposta acumula números (e virgula)
     }
-    resp.innerText = resposta                   // exibe a resposta
+    resp.innerText = resposta                    // exibe a resposta
 })
