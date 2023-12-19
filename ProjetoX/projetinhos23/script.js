c717fb6f545d3a7d971b92c8eb3273a9db0a2e44
Lucio Costa <luciosdc@outlook.com>
2023-12-19 01:57:23 -0300

    estudando java
    estudando

--- ProjetoX/projetinhos23/script.js
+++ ProjetoX/projetinhos23/script.js
@@ -4,10 +4,10 @@ const resp = document.querySelector("h3")
 
 //"escsuta" evento submit do form
 frm.addEventListener("submit", (e) => {
-    e.preventDefalt()                           // evita o envio do form
+    e.preventDefault()                           // evita o envio do form
     const numero = Number(frm.inNumero.value)   // obtém número informado
-    let resposta = `Entre ${numero} e 1:`       // String para montar a resposta
-    for(let i = numero; i > 0; i = i - 1) {                 // cria um for decrescente
+    let resposta = `Entre ${numero} e 1: `       // String para montar a resposta
+    for (let i = numero; i > 0; i = i - 1) {     // cria um for decrescente
         resposta = resposta + i + ", "           // resposta acumula números (e virgula)
     }
     resp.innerText = resposta                    // exibe a resposta
