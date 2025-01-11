d3045b56e5e6c665c47e28a1e98065d244296e7e
Lucio Costa <luciosdc@outlook.com>
2025-01-11 17:45:48 -0300

    lançadordedados
    programa dados

--- ProjetoX/projetinhos25/dice.js
+++ ProjetoX/projetinhos25/dice.js
@@ -1,1 +1,37 @@
-var sorteio = Math.floor(Math.random()) *  9 
+var sorteio = Math.floor(Math.random() * 10 + 1);
+
+switch (sorteio) {
+  case 1:
+    document.getElementById("face.png") .src = "d";
+    break;
+  case 2:
+    document.getElementById("face.png" .src = "Dice_Rage_Failure.png";
+    break;
+  case 3:
+    document.getElementById("face.png") .src = "Dice_Rage_Failure.png";
+    break;
+  case 4:
+    document.getElementById("face.png") .src = "Dice_Rage_Failure.png";
+    break;
+  case 5:
+    document.getElementById("face.png") .src = "Dice_Rage_Failure.png";
+    break;
+  case 6:
+    document.getElementById("face.png") .src = "Dice_Rage_Success.png";
+    break;
+  case 7:
+    document.getElementById("face.png") .src = "Dice_Rage_Success.png";
+    break;
+  case 8:
+    document.getElementById("face.png") .src = "Dice_Rage_Success.png";
+    break;
+  case 9:
+    document.getElementById("face.png") .src = "Dice_Rage_Success.png";
+    break;
+  case 10:
+    document.getElementById("face.png") .src = "Dice_Rage_Critical.png";
+    break;
+default:
+    //caso contrário
+    break;
+}
